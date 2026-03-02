0dba828190b6eb983cee942f3106e16d0142aa3c
veradec <glitchmill.dyj3d@simplelogin.co>
2026-03-02 23:45:05 +0530

    feat: loads and unloads the font && prints

--- holup.c
+++ holup.c
@@ -3,22 +3,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 const unsigned int x = 1920;
 const unsigned int y = 1080;
 
+
 void DrawCountDown(unsigned int sec){
+Font DreamerTM = LoadFont("assets/fonts/DreamerTM-Regular.ttf");
   while(!WindowShouldClose()){
     BeginDrawing();
     ClearBackground(BLACK);
     char buf[99];
     sprintf(buf, "%02d:%02d", sec/60, sec%60);
-    DrawText(buf, x/2.5, y/3, 120, RAYWHITE);
+    DrawTextEx(DreamerTM, buf, (Vector2){ 100.0f, 100.0f}, 100.0f,30,MAROON );
+
     sleep(1);
     sec--;
     EndDrawing();
   }  
+  UnloadFont(DreamerTM);
 }
-
 
 void DrawTimer(){
   unsigned int sec =0;
@@ -35,7 +39,6 @@ void DrawTimer(){
 }
 
 int main(int argc, char* argv[]){
-
   if(argc < 2){
     printf("USAGE: holdup [hold | timer] [5]\n");
     exit(1);
@@ -59,7 +62,7 @@ int main(int argc, char* argv[]){
   }
 
   CloseWindow();
-
+  
 
   return 0;
 }
